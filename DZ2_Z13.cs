4e7abc869cd4a1f5d9b14eda1fadb6e00780c499
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2023-01-23 18:39:48 +0300

    dz13

---
+++ DZ2_Z13.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int d = 0;
+
+if (number > 99)
+{
+    while (number >= 1000)
+    {
+        number /=10;
+
+    }
+    if (number >= 100 && number <999)
+    {
+        d = number % 10;
+    }
+    Console.WriteLine($"{d}");
+}
+else
+{
+    Console.WriteLine($"третьей цифры нет");
+}
