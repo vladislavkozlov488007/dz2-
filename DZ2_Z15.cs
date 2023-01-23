4ca914b187b928304841cac69d7529b0945ff2ae
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2023-01-23 18:40:14 +0300

    dz15

---
+++ DZ2_Z15.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите день недели от 1 до 7 : ");
+int day = Convert.ToInt32(Console.ReadLine());
+switch(day)
+{
+    case 1:
+      Console.WriteLine("понедельник - рабочий день");
+      break;
+    case 2:
+      Console.WriteLine("вторник - рабочий день");
+      break;
+    case 3:
+       Console.WriteLine("среда - рабочий день");
+      break;    
+    case 4:
+       Console.WriteLine("четверг - рабочий день");
+      break;  
+    case 5:
+       Console.WriteLine("пятница - рабочий день");
+      break;   
+    case 6:
+       Console.WriteLine("суббота - выходной день");
+      break; 
+    case 7:
+       Console.WriteLine("воскресенье - выходной день");
+      break;          
+    default:
+       Console.WriteLine("день недели не найден");
+       break;    
+}
+    
