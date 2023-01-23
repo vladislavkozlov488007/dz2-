7480039b22c28d1067721f0d97c1f289ac600d1a
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2023-01-23 18:39:12 +0300

    dz10

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Подходит так же для решения чисел больше 4
+Console.WriteLine ("Введите любое трехзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int d = 0;
+if (num > 99)
+{
+ while (num >= 1000)
+    {
+ num /= 10;
+    }
+ if (num >= 100 && num <= 999)
+    {
+ num /= 10;
+ d = num % 10;
+    }
+ Console.WriteLine($"{d}");
+}
+else
+
+ Console.WriteLine($"Третьей цифры нет");
